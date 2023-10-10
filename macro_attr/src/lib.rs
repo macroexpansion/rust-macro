79fb6e7a928b2990af12a23022fb79dcb4b1a762
blueyellowpink <andrew@trasua.dev>
2023-10-11 02:35:42 +0700

    attribute macro update

--- macro_attr/src/lib.rs
+++ macro_attr/src/lib.rs
@@ -7,13 +7,7 @@ use syn::{self, Item};
 
 #[proc_macro_attribute]
 pub fn macro_attribute(attr: TokenStream, item: TokenStream) -> TokenStream {
-    println!("attr: {attr}");
-    println!("item: {item}");
-
-    // let attr_ast: Item = syn::parse(attr).unwrap();
-    // if let Item::Verbatim(tokens) = attr_ast {
-    //     println!("{}", 1);
-    // }
+    let attr = attr.to_string();
 
     let item_ast: Item = syn::parse(item).unwrap();
     let func = if let Item::Fn(func) = item_ast {
@@ -26,7 +20,7 @@ pub fn macro_attribute(attr: TokenStream, item: TokenStream) -> TokenStream {
     let fn_ident = func.sig.ident;
     let gen = quote! {
         fn #fn_ident() {
-            println!("this is {} function", stringify!(#fn_ident));
+            println!("this is {} function and attribute {}", stringify!(#fn_ident), stringify!(#attr));
         }
     };
     TokenStream::from(gen)
